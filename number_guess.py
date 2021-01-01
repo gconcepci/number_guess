39d2cda042668e4ec7abafc837bc2fa4d25244e1
Gabe <gconcepci@gmail.com>
2020-12-31 22:31:20 -0500

    Rewrote program to handle errors better and to be more responsive to different user inputs.

--- number_guess.py
+++ number_guess.py
@@ -1,77 +1,86 @@
 from random import randint
-
-#Our structure is as follows:
-#Program gets a random number.
-#User tries to guess number.
-#If number is within 15 digits to guess, Program says warm.
-#If number is not within 15 digits, program say cold.
-#User has 5 chances to guess.
-#If guessed correctly, program says congrats and tells you how many guesses it took.
-#If not guessed, program shows number and asks to play again.
-
-
-#Initialize the winning number.
+"""
+Our structure is as follows:
+Program gets a random number.
+User tries to guess number.
+If user quits, print a message saying bye. Try to make a shuffled list of different messages that can be printed.
+Program verifies that input is a number.
+If number is within 10 digits to guess, Program says warm.
+If number is not within 10 digits, program say cold.
+User has 8 chances to guess.
+If guessed correctly, program says congrats and tells you how many guesses it took.
+If not guessed, program shows number and asks to play again.
+"""
+# Initialize the winning number.
 secret_number = randint(1, 100)
-print("I'm thinking of a number between 1 and 100.")
-print("Try to guess the number. I'll let you know if you're close.")
-print("Enter 'q' at any time to quit.")
-
-#Our closeness ranges.
-warm = range(1,15)
-hot = range(1,5)
-
-#Set our flag for the while loop.
+secret_number_range = range(1,101)
+print("I'm thinking of a number between 1 and 100.\nTry to guess the number. I'll let you know if you're close.\nEnter 'q' at any time to quit.")
+print(secret_number)
+# Set our flag for the while loop.
 active = True
-
-#Set our counter for attempts.
-guess_attempts = 1
+first_guess = True
+# Set our counter for attempts.
+guess_attempts = 0
+guess = ""
+guess_distances = []
 
 while active:
-    #Sets user input to variable guess.
+    # Sets user input to variable guess.
     guess = input("What is your guess: ")
 
-    # If guess is q, flag is set to false and program terminates.
     if guess == "q":
-        active = False
+        print("Thanks for playing! Bye bye!")
+        break
 
-    elif guess != secret_number and guess_attempts == 5:
-        print("\nSorry, you ran out of guesses.")
-        print(f"The number was {secret_number}.")
-        active = False
-        
+    elif guess.isdigit() is False:
+        print("Your guess has to be a number!")
+        first_guess = True
+        continue 
 
-
-    #Otherwise the guess is converted to an integer to be used in the program correctly.
-    elif guess != "q":
+    elif guess.isdigit():
         guess = int(guess)
-        
-        #If guess is not the winning number, attempts is incremented by 1 and loop restarts.
-        if guess != secret_number:
+        guess_distance = abs(guess - secret_number)
+
+        if guess not in secret_number_range:
+            print("Not a valid guess. Number has to be between 1 and 100!")
+            first_guess = True
+            continue
+
+        elif guess in secret_number_range:
             guess_attempts += 1
-            
-            #This is  where program will tell you if you are warm or cold.
-            if abs(secret_number - guess) in warm:
-                print("You're warm!")
-                continue
-            if abs(secret_number - guess) not in warm:
-                print("Youre cold!")
-                continue
+            guess_distances.append(guess_distance) 
 
-        
-       
-        #If guess is the winning number, a congrats is printed with the attempts it took and program ends.
-        else:
-            #To make the win message more grammatically correct, the program checks if guess attempts
-            #is 1 or greater. If 1, the message will say 1 try. If more than 1, it will say # tries.
-            if guess_attempts == 1:
-                print(f"Congrats, you guessed the number in {guess_attempts} try!")
-                active = False
+            if first_guess is True and guess != secret_number:    
+                if guess_distances[0] < 10:
+                    print("You're warm!")
+                    first_guess = False                   
+                    continue
 
-            elif guess_attempts > 1:
-                print(f"Congrats, you guessed the number in {guess_attempts} tries!")
-                active = False
-            
+                elif guess_distances[0] > 10:
+                    print("You're cold!")
+                    first_guess = False
+                    continue
 
-            
-#Success! But can you refactor all this sloppy code and create functions for each step?
+            elif first_guess is False and guess != secret_number: 
+                if guess_distances[-1] < guess_distances[-2]:
+                    print("You're getting warmer!")
+                    continue
 
+                elif guess_distances[-1] > guess_distances[-2]:
+                    print("You're getting colder!")
+                    continue
+
+                else:
+                    print("You just guessed the same number!")
+            else:
+                if guess_attempts == 1:
+                    print(f"Congratulations! You guessed the number in {guess_attempts} try!")
+                    break
+
+                else:
+                    print(f"Congratulations! You guessed the number in {guess_attempts} tries!")
+                    break
+                
+    if guess_attempts >= 8:
+        print(f"Sorry you failed to guess the number.\nThe number was {secret_number}.")
+        active = False
